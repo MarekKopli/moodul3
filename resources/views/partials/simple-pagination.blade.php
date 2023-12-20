3cc22ead6311921779c0f69bcc8cac399f9aacaa
marek <marek.kopli@tptlive.ee>
2023-12-20 20:40:45 +0200

    daisyui-simplepagination

--- resources/views/partials/simple-pagination.blade.php
+++ resources/views/partials/simple-pagination.blade.php
@@ -1,3 +1,9 @@
+<style>
+    .pagination {
+        display: flex;
+    }
+
+</style>
 @if ($paginator->hasPages())
     <nav>
         <ul class="pagination">
@@ -5,12 +11,12 @@
             @if ($paginator->onFirstPage())
                 <li class="disabled" aria-disabled="true"><span>@lang('pagination.previous')</span></li>
             @else
-                <li><a href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
+                <li><a class="join-item btn btn-outline" href="{{ $paginator->previousPageUrl() }}" rel="prev">@lang('pagination.previous')</a></li>
             @endif
 
             {{-- Next Page Link --}}
             @if ($paginator->hasMorePages())
-                <li><a href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
+                <li><a class="join-item btn btn-outline" href="{{ $paginator->nextPageUrl() }}" rel="next">@lang('pagination.next')</a></li>
             @else
                 <li class="disabled" aria-disabled="true"><span>@lang('pagination.next')</span></li>
             @endif
